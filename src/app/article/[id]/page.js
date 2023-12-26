503cfe9aa18c8991bad9562f4f528114d5aa990f
TheThing12 <safarazcarra2008@gmail.com>
2023-12-26 20:36:22 +0700

    coba

--- src/app/article/[id]/page.js
+++ src/app/article/[id]/page.js
@@ -1,6 +1,6 @@
 "use client";
-import "./article.css";
-import ResponsiveAppBar from "@/app/Components/navbar";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faFacebook,
@@ -9,33 +9,27 @@ import {
   faTwitter,
   faXTwitter,
 } from "@fortawesome/free-brands-svg-icons";
-import axios from "axios";
-import { useState, useEffect } from "react";
-
-export async function generateArticle() {
-  const res = await axios.get(
-    "http://localhost:1337/api/articles?populate=image"
-  );
-  const data = await res.json();
-
-  return data.map((article) => ({
-    id: article.id,
-  }));
-}
+import ResponsiveAppBar from "@/app/Components/navbar";
+import "./article.css";
 
 export default function Article() {
-  const [article, setArticle] = useState("");
+  const [article, setArticle] = useState([]);
 
   useEffect(() => {
-    async function fetchArticle(id) {
-      const getUrl = `http://localhost:1337/api/articles${id}`;
-      const response = await axios.get(getUrl);
-      const result = response.data.data;
-      console.log(getUrl);
-      return setArticle(result);
+    async function fetchArticle() {
+      try {
+        const getUrl = "http://localhost:1337/api/articles"; // Removed `populate` query parameter
+        const response = await axios.get(getUrl);
+        const result = response.data.data;
+        console.log(getUrl);
+        setArticle(result);
+      } catch (error) {
+        console.error("Error fetching articles:", error.message);
+      }
     }
     fetchArticle();
   }, []);
+
   return (
     <div className="container-article">
       <section>
@@ -43,16 +37,17 @@ export default function Article() {
       </section>
       {article &&
         article.map((data, i) => {
-          console.log(data.i);
+          console.log(data); // Removed incorrect usage of data.i
           return (
-            <div>
+            <div key={i}>
+              {" "}
+              {/* Added a unique key for each mapped element */}
               <div className="div-container">
                 <p className="title">{data.attributes.title}</p>
                 <div className="container-author-date">
                   <p className="author">{data.attributes.author}</p>
                   <p className="date">{data.attributes.date}</p>
                 </div>
-
                 <p className="article">{data.attributes.article}</p>
               </div>
             </div>
@@ -61,19 +56,19 @@ export default function Article() {
       <footer className="footer">
         <div className="footerContainer">
           <div className="sociallinks">
-            <a href="#" class="linkedin">
+            <a href="#" className="linkedin">
               <FontAwesomeIcon icon={faXTwitter} size="2xs" width={16} />
             </a>
-            <a href="#" class="facebook">
+            <a href="#" className="facebook">
               <FontAwesomeIcon icon={faFacebook} size="2xs" width={16} />
             </a>
-            <a href="#" class="instagram">
+            <a href="#" className="instagram">
               <FontAwesomeIcon icon={faInstagram} size="2xs" width={16} />
             </a>
-            <a href="#" class="github">
+            <a href="#" className="github">
               <FontAwesomeIcon icon={faGithub} size="2xs" width={16} />
             </a>
-            <a href="#" class="twitter">
+            <a href="#" className="twitter">
               <FontAwesomeIcon icon={faTwitter} size="2xs" width={16} />
             </a>
           </div>
